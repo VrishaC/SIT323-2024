08a92e5cd034faf4a2cdd9c4292efcca38e074fa
VrishaC <140986857+VrishaC@users.noreply.github.com>
2024-04-01 14:03:45 +1100

    Week 4

---
+++ Week 4/Task4.2C/calculation.js
@@ -0,0 +1,223 @@
+const { error } = require("console");
+const express= require("express");
+const res = require("express/lib/response");
+const app= express();
+const fs = require('fs');
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'calculate-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
+  
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+const add= (n1,n2) => {
+    return n1+n2;
+}
+
+const subtract= (n1,n2) => {
+  return n1-n2;
+}
+
+const mult= (n1,n2) => {
+  return n1*n2;
+}
+
+const div= (n1,n2) => {
+  if(n2 == 0)
+  {
+    throw new Error("Division by zero is not applicable!");
+  }
+  return n1/n2;
+}
+
+const power= (n1,n2) => {
+  return Math.pow(n1,n2);
+}
+
+const sqrt= (n1) => {
+  if(n1 < 0)
+  {
+    throw new Error("Square root of negative numbers are not applicable!")
+  }
+  return Math.sqrt(n1);
+}
+
+const mod= (n1,n2) => {
+  if (n2 == 0)
+  {
+    throw new Error("Division by zero is not applicable!");
+  }
+  return n1%n2;
+}
+
+app.get("/add", (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = add(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/subtract", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for subtraction');
+  const result = subtract(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/mult", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
+  const result = mult(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/div", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for division');
+  const result = div(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/power", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for exponentiation');
+  const result = power(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/sqrt", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+    
+  logger.info('Parameter '+n1+' received for square root operations');
+  const result = sqrt(n1);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+app.get("/mod", (req,res)=>{
+  try{
+  const n1= parseFloat(req.query.n1);
+  const n2=parseFloat(req.query.n2);
+  if(isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+  }
+  if(isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+  }
+  
+  logger.info('Parameters '+n1+' and '+n2+' received for modulus operations');
+  const result = mod(n1,n2);
+  res.status(200).json({statuscocde:200, data: result }); 
+  } catch(error) { 
+      console.error(error)
+      res.status(500).json({statuscocde:500, msg: error.toString() })
+    }
+});
+
+const port=3040;
+app.listen(port,()=> {
+    console.log("hello i'm listening to port"+port);
+})
